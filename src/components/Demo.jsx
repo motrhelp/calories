c5f9d91c69067d75c88a1f178ccf89cb9a1f92b7
Stan K <motrhelp@stans-air.home>
2022-07-01 18:32:03 +0200

    Introduce record selection

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import { Accordion, AccordionDetails, AccordionSummary, AppBar, Autocomplete, Button, Card, CardActions, CardContent, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Fab, Grid, IconButton, Slide, TextField, Toolbar, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, AppBar, Autocomplete, Button, Checkbox, Grid, IconButton, TextField, Toolbar, Typography } from '@mui/material';
 
 import { ArrowBackIos } from '@mui/icons-material';
 import AddIcon from '@mui/icons-material/Add';
+import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -116,8 +117,12 @@ export default function Demo() {
 
     const [records, setRecords] = React.useState();
     const [food, setFood] = React.useState();
-    const [test, setTest] = React.useState(null);
+    const [adding, setAdding] = React.useState(null);
+    const [addingAmount, setAddingAmount] = React.useState();
     const [expanded, setExpanded] = React.useState(false);
+    const [showBottomMenu, setShowBottomMenu] = React.useState(false);
+    const [selecting, setSelecting] = React.useState(false);
+    const [selectedRecords, setSelectedRecords] = React.useState([]);
 
     React.useEffect(() => {
         setRecords(loadRecords());
@@ -125,102 +130,24 @@ export default function Demo() {
     }, [])
 
     const handleAccordionChange = index => (event, isExpanded) => {
-        setExpanded(isExpanded ? index : false);
+        if (selecting) {
+            if (selectedRecords.includes(index)) {
+                setSelectedRecords(selectedRecords.filter(i => i !== index));
+            } else {
+                setSelectedRecords([...selectedRecords, index]);
+            }
+        } else {
+            let newExpanded = isExpanded ? index : false
+            setExpanded(newExpanded);
+            setShowBottomMenu(newExpanded);
+        }
     }
 
-    function Row({ record, index }) {
-        return (
-            <Accordion
-                key={index}
-                expanded={expanded === index}
-                onChange={handleAccordionChange(index)}
-            >
-                <AccordionSummary
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Stack
-                        direction={'row'}
-                        sx={{
-                            width: '100%',
-                        }}>
-                        {
-                            record.disabled ?
-                                <AddIcon
-                                    sx={{ width: '100%' }}
-                                />
-                                : null
-                        }
-                        {record && !record.disabled ?
-                            <React.Fragment>
-                                <Typography variant='subtitle2' sx={{ width: '80%' }}>{record.title}</Typography>
-                                <Typography variant='body2' sx={{ width: '10%', alignSelf: 'center' }}>{record.amount}</Typography>
-                                <Typography variant='subtitle2' sx={{ width: '10%', alignSelf: 'center', textAlign: 'right' }}>
-                                    {Math.round(record.amount / 100 * record.calories)}
-                                </Typography>
-                            </React.Fragment>
-                            : null}
-                    </Stack>
-                </AccordionSummary>
-                <AccordionDetails>
-                    {record.disabled ?
-                        <React.Fragment>
-                            <Autocomplete
-                                disablePortal
-                                options={food.filter((food) => food.title != null)}
-                                renderInput={(params) => <TextField {...params} label="Food" />}
-                                getOptionLabel={(food) => food.title}
-                                onChange={(event, newValue) => {
-                                    setTest(newValue.title);
-                                }}
-                                sx={{
-                                    width: '100%',
-                                    paddingBottom: 2
-                                }}
-                            />
-                            <Stack
-                                direction={'row'}
-                                sx={{
-                                    width: '100%',
-                                }}>
-                                <React.Fragment>
-                                    <Typography variant='subtitle2' sx={{ width: '80%' }}>{test}</Typography>
-                                    <Typography variant='body2' sx={{ width: '10%', alignSelf: 'center' }}>{1}</Typography>
-                                    <Typography variant='subtitle2' sx={{ width: '10%', alignSelf: 'center', textAlign: 'right' }}>
-                                        {169}
-                                    </Typography>
-                                </React.Fragment>
-                            </Stack>
-                        </React.Fragment>
-                        :
-                        <TableContainer component={Paper}>
-                            <Table sx={{ width: '100%' }} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="center">Protein</TableCell>
-                                        <TableCell align="center">Fat</TableCell>
-                                        <TableCell align="center">Carbs</TableCell>
-                                        <TableCell align="center">Calories</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow
-                                        key={record.name}
-                                    // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell align="center">10</TableCell>
-                                        <TableCell align="center">12</TableCell>
-                                        <TableCell align="center">1</TableCell>
-                                        <TableCell align="center">10</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    }
-
-                </AccordionDetails>
-            </Accordion>
-        )
+    // Check if the difference between two dates is smaller than 10 seconds
+    const isCloseToNow = (date) => {
+        const now = new Date();
+        const diff = Math.abs(now - date);
+        return diff < 10000;
     }
 
     return (
@@ -245,15 +172,20 @@ export default function Demo() {
             </AppBar>
 
             {records?.map((record, index) =>
-
                 <Accordion
                     key={index}
                     expanded={expanded === index}
                     onChange={handleAccordionChange(index)}
+                    sx={{
+                        backgroundColor: selecting ? (selectedRecords.includes(index) ? 'info.light' : null) : null,
+                    }}
                 >
                     <AccordionSummary
                         aria-controls="panel1a-content"
                         id="panel1a-header"
+                        sx={{
+                            backgroundColor: isCloseToNow(record.created) ? 'success.light' : null
+                        }}
                     >
                         <Stack
                             direction={'row'}
@@ -269,7 +201,7 @@ export default function Demo() {
                             }
                             {record && !record.disabled ?
                                 <React.Fragment>
-                                    <Typography variant='subtitle2' sx={{ width: '80%' }}>{record.title}</Typography>
+                                    <Typography variant='subtitle2' sx={{ width: '80%', alignSelf: 'center' }}>{record.title}</Typography>
                                     <Typography variant='body2' sx={{ width: '10%', alignSelf: 'center' }}>{record.amount}</Typography>
                                     <Typography variant='subtitle2' sx={{ width: '10%', alignSelf: 'center', textAlign: 'right' }}>
                                         {Math.round(record.amount / 100 * record.calories)}
@@ -283,32 +215,50 @@ export default function Demo() {
                             <Grid container spacing={2}>
                                 <Grid item xs={12}>
                                     <Autocomplete
-                                        disablePortal
                                         options={food.filter((food) => food.title != null)}
                                         renderInput={(params) => <TextField {...params} label="Food" />}
                                         getOptionLabel={(food) => food.title}
                                         onChange={(event, newValue) => {
-                                            setTest(newValue.title);
+                                            setAdding(food.find((food) => food.title === newValue.title));
                                         }}
                                     />
                                 </Grid>
-                                {test ?
+                                {adding ?
                                     <React.Fragment>
-                                        <Grid item xs={8}>
-                                            <Typography variant='subtitle2'>{test}</Typography>
+                                        <Grid item xs={7} sx={{ alignSelf: 'center' }}>
+                                            <Typography variant='subtitle2'>{adding.title}</Typography>
                                         </Grid>
-                                        <Grid item xs={2}>
-                                            <Typography variant='body2' sx={{ alignSelf: 'center' }}>{1}</Typography>
+                                        <Grid item xs={3} sx={{ alignSelf: 'center' }}>
+                                            <TextField
+                                                size='small'
+                                                label="Amount"
+                                                value={addingAmount}
+                                                onChange={(event) => {
+                                                    setAddingAmount(event.target.value);
+                                                }}
+                                                type="number"
+                                            />
                                         </Grid>
-                                        <Grid item xs={2}>
-                                            <Typography variant='subtitle2' sx={{ alignSelf: 'center', textAlign: 'right' }}>
-                                                {169}
+                                        <Grid item xs={2} sx={{ alignSelf: 'center', textAlign: 'right' }}>
+                                            <Typography variant='subtitle2'>
+                                                {addingAmount ?
+                                                    Math.round(addingAmount / 100 * adding.calories)
+                                                    : 0
+                                                }
                                             </Typography>
                                         </Grid>
                                         <Grid item xs={10} />
                                         <Grid item xs={2}>
                                             <Button onClick={() => {
-                                                setTest(null);
+                                                records.splice(index, 0, {
+                                                    title: adding.title,
+                                                    amount: addingAmount,
+                                                    calories: adding.calories,
+                                                    created: new Date(),
+                                                })
+
+                                                setAddingAmount(null);
+                                                setAdding(false);
                                                 setExpanded(false);
                                             }}>
                                                 ADD
@@ -346,6 +296,31 @@ export default function Demo() {
                     </AccordionDetails>
                 </Accordion>
             )}
+
+            {showBottomMenu ?
+                <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
+                    <Toolbar sx={{ justifyContent: 'space-evenly', }}>
+                        <IconButton color="inherit">
+                            <DeleteIcon />
+                        </IconButton>
+                        <IconButton color="inherit"
+                            onClick={() => {
+                                if (selecting) {
+                                    setSelectedRecords([]);
+                                    setSelecting(false);
+                                } else {
+                                    setSelecting(true);
+                                    setSelectedRecords([expanded]);
+                                    setExpanded(false);
+                                }
+                            }}>
+                            <LibraryAddCheckIcon
+                            />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                : null
+            }
 
         </Box>
     );
